278fba0a7bf2b1add58a8c0dbc33ea38397fcc19
joselugb <joselugb@gmail.com>
2021-12-15 00:07:00 -0500

    resuelto
    resuelto

--- Python/Who-likes-it.py
+++ Python/Who-likes-it.py
@@ -16,4 +16,33 @@
 
 def likes(names):
     # your code here
-    pass
+    cadena = ""
+    if (len(names) == 0):
+        cadena = "no one likes this"
+    elif (len(names) == 1):
+        cadena = names[0]+" likes this"
+    elif (len(names) == 2):
+        cadena = names[0]+" and "+names[1]+" like this"
+    elif (len(names) == 3):
+        cadena = names[0]+", "+names[1]+" and "+names[2]+" like this"
+    elif (len(names) >= 4):
+        cadena = names[0]+", "+names[1]+" and "+str(len(names)-2)+" others like this"
+    
+    
+    return cadena
+
+
+
+##import codewars_test as test
+##from solution import likes
+##
+##@test.it('Basic tests')
+##def _():
+##    test.assert_equals(likes([]), 'no one likes this')
+##    test.assert_equals(likes(['Peter']), 'Peter likes this')
+##    test.assert_equals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
+##    test.assert_equals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
+##    test.assert_equals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
+## likes(['Sylia Stingray', 'Brian J. Mason', 'Daley Wong', 'Linna Yamazaki', 'Largo', 'Macky Stingray', 'Nigel', 'Quincy Rosenkreutz', 'Galatea'])
+## likes(['Priscilla S. Asagiri', 'Anri', 'Leon McNichol', 'Brian J. Mason', 'Sylia Stingray', 'Galatea', 'Quincy Rosenkreutz', 'Macky Stingray', 'Daley Wong', 'Sylvie', 'Nigel', 'Linna Yamazaki'])
+## likes(['Nene Romanova', 'Nigel', 'Quincy Rosenkreutz', 'Anri', 'Sylvie', 'Linna Yamazaki', 'Largo', 'Galatea', 'Leon McNichol', 'Macky Stingray', 'Brian J. Mason', 'Priscilla S. Asagiri', 'Sylia Stingray'])
